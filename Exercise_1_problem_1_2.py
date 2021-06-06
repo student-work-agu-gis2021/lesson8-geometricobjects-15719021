b66ebf763ea01a2d563dcece4f1235d9ce9942f5
15719021 <>
2021-06-06 16:43:52 +0000

    I added lines to create def lines.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -18,7 +18,7 @@ def create_point_geom(x_coord,y_coord):
   """
   point = point(x_coord,y_coord)
   return(point)
-  
+
 # Test your function by running these code cells:
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -34,6 +34,15 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
+def create_line_geom(points):
+  """
+  prameter:points
+  purpose:create line
+  """
+  assert type(points)=="Input should be a list!"
+  assert len(points)>=2, "LineString object requires at least two points!"
+  line = LineString([points[0],points[1]])
+  return line
 
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
